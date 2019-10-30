829cd3b3e2e355e0c5be32ddf435eefa6a3a676b
krisn64209 <krisnorris@gmail.com>
2019-10-30 14:26:54 +0000

    added web helper to DRY code

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,10 +2,7 @@ require './app.rb'
 
 feature 'enter two player names' do
   scenario 'can enter the names of two players' do
-    visit('/')
-    fill_in :player_1_name, with: 'Kris'
-    fill_in :player_2_name, with: 'Marc'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Kris vs. Marc'
   end
 end
